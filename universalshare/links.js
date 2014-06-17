fe7c81582682569c1c8733e625e1b864a45d7865
Terry Yuen <mocking+expando@gmail.com>
2014-06-17 19:55:28 +0800

    Update links.js

--- universalshare/links.js
+++ universalshare/links.js
@@ -12,6 +12,7 @@ return(o?({".":".com",".o":".org",".n":".net"})[o]:"")+(w||"")})
 "LinkedIn .linkedin./shareArticle?mini=true&url=@u&title=@t&summary=@s",
 "Reddit reddit./submit?url=@u&title=@t",
 "Technorati technorati./faves?add=@u",
+"Blogger .blogger./blog_this.pyra?t=@t&u=@u",
 "Digg digg./submit?phase=2&url=@u&title=@t&bodytext=@s",
 "Delicious delicious./post?url=@u&title=@t&notes=@s",
 "Fark cgi.fark./cgi/fark/farkit.pl?h=@t&u=@u",
@@ -19,6 +20,8 @@ return(o?({".":".com",".o":".org",".n":".net"})[o]:"")+(w||"")})
 "MySpace .myspace./Modules/PostTo/Pages/?u=@u&t=@t",
 "Mister%20Wong .mister-wong./addurl/?bm_url=@u&bm_description=@t",
 "NetVibes .netvibes./share?title=@t&url=@u",
-"NewsVine .newsvine./_tools/seed&save?u=@u&h=@t"
+"NewsVine .newsvine./_tools/seed&save?u=@u&h=@t",
+"Evernote .evernote./clip.action?url=@u&title=@t",
+"Wanelo wanelo./p/post?bookmarklet=&source=toolbar&title=@t&url=@u"
 
 ],0,{}));
